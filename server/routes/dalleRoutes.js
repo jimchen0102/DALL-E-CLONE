6ceff328b2057e323427d2fb83def19c18696f11
jim.chen <azx011014@gmail.com>
2023-11-18 14:05:05 +0800

    [adjust]: adjust server status

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -13,7 +13,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-  res.send("Hello from DALL-E!");
+  res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
 router.route("/").post(async (req, res) => {
@@ -28,10 +28,9 @@ router.route("/").post(async (req, res) => {
     });
 
     const image = aiResponse.data.data[0].b64_json;
-
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.status(500).send(error?.response.data.error.message);
   }
 });
