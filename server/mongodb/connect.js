fb35b53aa0eaa9fbe2ecf2309a8a60981c7c54ce
jim.chen <azx011014@gmail.com>
2023-11-18 14:26:06 +0800

    [feature]: add create post api

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -5,7 +5,7 @@ const connectDB = (url) => {
   mongoose
     .connect(url)
     .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.log(err));
+    .catch((error) => console.log(error));
 };
 
 export default connectDB;
